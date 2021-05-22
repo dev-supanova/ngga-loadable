a6d1b454b0289adf158204f04b50030f893f3791
Alec Agisanyang <aagisanyang@bitri.co.bw>
2021-05-22 15:17:31 +0200

    Default loader

---
+++ projects/ngga-loadable/src/lib/loaders/default/default-loader.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from "@angular/core/testing";
+import { DefaultLoaderComponent } from "./default-loader.component";
+
+describe('DefaultLoaderComponent', () => {
+    let component: DefaultLoaderComponent;
+    let fixture: ComponentFixture<DefaultLoaderComponent>;
+
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            declarations: [DefaultLoaderComponent]
+        }).compileComponents();
+    });
+
+    beforeEach(() => {
+        fixture = TestBed.createComponent(DefaultLoaderComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
+
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
+});
